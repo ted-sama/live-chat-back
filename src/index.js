7095c3e228ca60155b21e9b3a812c2cdd1d3fa03
Teddy <teddynsoki@gmail.com>
2025-02-05 15:59:57 +0100

    added duration to items and video by link

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import http, { get } from "http";
+import http from "http";
 import cors from "cors";
 import path from "path";
 import fs from "fs";
@@ -14,31 +14,33 @@ const ENV_FILE = `.env.${process.env.NODE_ENV || 'local'}`;
 dotenv.config({ path: ENV_FILE });
 
 // Clear upload folder on server start
-// const directory = path.join(
-//   path.dirname(fileURLToPath(import.meta.url)),
-//   "../uploads"
-// );
-// fs.readdir(directory, (err, files) => {
-//   if (err) throw err;
+const directory = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  "../uploads"
+);
+fs.readdir(directory, (err, files) => {
+  if (err) throw err;
 
-//   for (const file of files) {
-//     fs.unlink(path.join(directory, file), (err) => {
-//       if (err) throw err;
-//     });
-//   }
-// });
+  for (const file of files) {
+    fs.unlink(path.join(directory, file), (err) => {
+      if (err) throw err;
+    });
+  }
+});
 
 const __filename = fileURLToPath(import.meta.url); // Convertir l'URL du module en chemin de fichier
 const __dirname = path.dirname(__filename); // Obtenir le dossier du fichier
 
 // Queue logic
 const queue = [];
+let isPlaying = false;
 
-export const addToQueue = ({ type, src, caption }) => {
+export const addToQueue = ({ type, src, caption, duration }) => {
   queue.push({
     type,
     src,
     caption,
+    duration,
   });
 
   console.log("Item added to queue:", queue);
@@ -48,9 +50,7 @@ export const addToQueue = ({ type, src, caption }) => {
   }
 };
 
-let isPlaying = false;
-
-const playQueue = () => {
+const playQueue = async () => {
   console.log("playQueue: Queue length", queue.length);
   console.log("Queue:", queue);
 
@@ -70,7 +70,7 @@ const playQueue = () => {
   const item = queue.shift();
   console.log("Playing:", item); // Devrait s'afficher quand un élément est pris en charge
 
-  let delay = 6500; // Par défaut, attendre 5 secondes
+  let delay = 0; // Initialiser le délai à 0
 
   isPlaying = true; // Marque l'état comme étant "en train de jouer"
 
@@ -79,12 +79,28 @@ const playQueue = () => {
     console.log("Item to play:", item); // Assure-toi que l'item est bien un objet valide
     io.emit("play", item);
 
+    // Si c'est une image, ajuster le délai selon la durée de l'image
+    if (item.type === "image") {
+      delay = item.duration + 1000; // Durée image + 1 seconde
+      console.log("Image duration:", item.duration);  
+    }
     // Si c'est une vidéo, ajuster le délai selon la durée de la vidéo
-    if (item.type === "video") {
-      getVideoDuration(item.src).then((duration) => {
+    else if (item.type === "video") {
+      let duration = 0; // Initialiser la durée à 0
+
+      try {
+        duration = await getVideoDuration(item.src);
         console.log("Video duration:", duration);
+      } catch (error) {
+        console.error("Error getting video duration:", error);
+      }
+
+      if (item.duration === 0) {
         delay = duration * 1000 + 1000; // Durée vidéo + 1 seconde
-      });
+      } else {
+        // delay = item.duration >= duration * 1000 ? duration * 1000 + 1000 : item.duration + 1000;
+        delay = item.duration + 1000;
+      }
     }
   }
 
@@ -119,7 +135,7 @@ app.use("/api/upload", uploadRouter);
 
 // Socket.io
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log(`user connected: ${socket.id}`);
 
   socket.on("disconnect", () => {
     console.log(`user disconnected: ${socket.id}`);
--- src/routes/upload.js
+++ src/routes/upload.js
@@ -17,33 +17,40 @@ const router = Router();
 const __filename = fileURLToPath(import.meta.url); // Convertir l'URL du module en chemin de fichier
 const __dirname = path.dirname(__filename); // Obtenir le dossier du fichier
 
+// duration is in milliseconds
+// default duration for images is 5 seconds, for videos is 0 (video original duration)
+
 // POST /api/upload/image-by-file
 router.post("/image-by-file", upload.single("src"), (req, res) => {
   if (!req.file) return res.status(400).json({ error: "No file" });
 
   const src = `${process.env.SERVER_URL}/uploads/${req.file.filename}`;
   const caption = req.body.caption || "";
-  addToQueue({ type: "image", src, caption });
+  const duration = parseInt(req.body.duration) || 5000;
+  addToQueue({ type: "image", src, caption, duration });
 
   res.json({
     success: true,
     src,
     caption,
+    duration
   });
 });
 
 // POST /api/upload/image-by-link
 router.post("/image-by-link", (req, res) => {
   const src = req.body.src;
-  if (!src) return res.status(400).json({ error: "No file" });
+  if (!src) return res.status(400).json({ error: "No src" });
 
   const caption = req.body.caption || "";
-  addToQueue({ type: "image", src, caption });
+  const duration = parseInt(req.body.duration) || 5000;
+  addToQueue({ type: "image", src, caption, duration });
 
   res.json({
     success: true,
     src,
     caption,
+    duration
   });
 });
 
@@ -53,12 +60,31 @@ router.post("/video-by-file", upload.single("src"), (req, res) => {
 
   const src = `${process.env.SERVER_URL}/uploads/${req.file.filename}`;
   const caption = req.body.caption || "";
-  addToQueue({ type: "video", src, caption });
+  const duration = parseInt(req.body.duration) || 0;
+  addToQueue({ type: "video", src, caption, duration });
 
   res.json({
     success: true,
     src,
     caption,
+    duration
+  });
+});
+
+// POST /api/upload/video-by-link
+router.post("/video-by-link", (req, res) => {
+  const src = req.body.src;
+  if (!src) return res.status(400).json({ error: "No src" });
+
+  const caption = req.body.caption || "";
+  const duration = parseInt(req.body.duration) || 0;
+  addToQueue({ type: "video", src, caption, duration });
+
+  res.json({
+    success: true,
+    src,
+    caption,
+    duration
   });
 });
 
@@ -69,6 +95,7 @@ router.post("/video-by-link/youtube", async (req, res) => {
     return res.status(400).json({ error: "YouTube video URL is required" });
 
   const caption = req.body.caption || "";
+  const duration = parseInt(req.body.duration) || 0;
 
   try {
     // Download the video using ytdl-core
@@ -93,12 +120,15 @@ router.post("/video-by-link/youtube", async (req, res) => {
         type: "video",
         src: `${process.env.SERVER_URL}/uploads/${path.basename(videoPath)}`,
         caption,
+        duration,
       });
       res
         .status(200)
         .json({
           success: true,
           src: `${process.env.SERVER_URL}/uploads/${path.basename(videoPath)}`,
+          caption,
+          duration,
         });
     });
 
